8089fc97d939b47b18a1731e9fd1f16e6aa2d085
BlakeAghili <peopleofksmo@gmail.com>
2021-06-15 20:02:02 -0400

    some more

---
+++ bookables/src/components/Bookables/BookablesList.js
@@ -0,0 +1,24 @@
+import { bookables } from "../../static.json";
+import { useState } from "react";
+
+export default function BookablesList() {
+  const group = "Rooms";
+  const bookablesInGroup = bookables.filter((b) => b.group === group);
+  const [bookableIndex, setBookableIndex] = useState(1);
+
+  function changeBookable(selectedIndex) {
+    setBookableIndex(selectedIndex);
+    console.log(selectedIndex);
+  }
+  return (
+    <ul className="bookables items-list-nav">
+      {bookablesInGroup.map((b, i) => (
+        <li key={b.id} className={i === bookableIndex ? "selected" : null}>
+          <button className="btn" onClick={() => changeBookable(i)}>
+            {b.title}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
